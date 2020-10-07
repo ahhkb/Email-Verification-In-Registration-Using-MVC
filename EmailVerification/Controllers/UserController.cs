8e636238c4caba889a9b6a07df7b4da3c21bb9f9
Ali Hussain Hiader <alihussainhaider5@gmail.com>
2020-10-07 11:58:59 +0500

    Donw wiht the registration design

--- EmailVerification/Controllers/UserController.cs
+++ EmailVerification/Controllers/UserController.cs
@@ -13,5 +13,10 @@ namespace EmailVerification.Controllers
         {
             return View();
         }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
     }
 }
